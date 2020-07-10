72def6a032e71e0f7e285713dd7654b140ab57fa
Iván Ovejero <ivov.src@gmail.com>
2020-07-09 22:34:52 -0300

    Add automatic file placement

---
+++ FilePlacer.ts
@@ -0,0 +1,109 @@
+import fs from "fs";
+import { join } from "path";
+import { promisify } from "util";
+
+const relocate = promisify(fs.rename);
+
+export default class FilePlacer {
+  // /output at nodemaker
+  sourceDirPath: string;
+
+  // files in /output in nodemaker
+  sourceFilenames: string[];
+
+  // /packages/nodes-base/ at n8n
+  destinationNodesBaseDir: string;
+
+  // /packages/nodes-base/nodes at n8n
+  destinationNodesDir: string;
+
+  // /packages/nodes-base/credentials at n8n
+  destinationCredentialsDir: string;
+
+  constructor() {
+    this.sourceDirPath = join(__dirname, "..", "output");
+    this.sourceFilenames = fs.readdirSync(this.sourceDirPath);
+    this.destinationNodesBaseDir = join(
+      __dirname,
+      "..",
+      "..",
+      "n8n",
+      "packages",
+      "nodes-base"
+    );
+    this.destinationNodesDir = join(this.destinationNodesBaseDir, "nodes");
+    this.destinationCredentialsDir = join(
+      this.destinationNodesBaseDir,
+      "credentials"
+    );
+  }
+
+  /**Place `package.json`, credential file, and node files in their appropriate locations in the n8n repo.*/
+  public async run() {
+    await this.placePackageJson();
+    await this.placeCredentialFile();
+    await this.placeNodeFiles();
+  }
+
+  private async placePackageJson() {
+    const source = join(this.sourceDirPath, "package.json");
+    const destination = join(this.destinationNodesBaseDir, "package.json");
+
+    await relocate(source, destination);
+  }
+
+  /**Place credential file (`*.credentials.ts`) in the appropriate location in the n8n repo.*/
+  private async placeCredentialFile() {
+    const credentialsFilename = this.sourceFilenames.find((file) =>
+      file.endsWith(".credentials.ts")
+    );
+
+    if (!credentialsFilename) {
+      throw Error("No source *.credentials.ts file found!");
+    }
+
+    const source = join(this.sourceDirPath, credentialsFilename);
+
+    const destination = join(
+      this.destinationCredentialsDir,
+      credentialsFilename
+    );
+
+    await relocate(source, destination);
+  }
+
+  /**Place node files (`*.node.ts`, `GenericFunctions.ts`, and resource files if any) in their appropriate location in the n8n repo.*/
+  private async placeNodeFiles() {
+    const nodeFilenames = this.sourceFilenames.filter(
+      (file) => file !== "package.json" && !file.endsWith(".credentials.ts")
+    );
+
+    const destination = join(
+      this.destinationNodesDir,
+      this.getDestinationDirname()
+    );
+
+    if (!fs.existsSync(destination)) {
+      fs.mkdirSync(destination);
+    }
+
+    nodeFilenames.forEach(async (filename) => {
+      const source = join(this.sourceDirPath, filename);
+      const fileDestination = join(destination, filename);
+      await relocate(source, fileDestination);
+    });
+  }
+
+  /**Returns the name of the service, to be used as a dirname.*/
+  private getDestinationDirname() {
+    const serviceFilename = this.sourceFilenames.find((file) =>
+      file.endsWith(".node.ts")
+    );
+
+    if (!serviceFilename) {
+      throw Error("No source *.node.ts file found!");
+    }
+
+    return serviceFilename.replace(".node.ts", "");
+  }
+}
