179cc2107d9e94e145770d094cc4164895060338
Iván Ovejero <ivov.src@gmail.com>
2020-07-20 17:24:18 -0300

    Convert icon prompter to async/await

--- scripts/resizeImage.ts
+++ scripts/resizeImage.ts
@@ -1,16 +1,17 @@
 import inquirer from "inquirer";
 import ImageResizer from "../utils/ImageResizer";
 
-inquirer
-  .prompt([
-    {
-      name: "iconToResize",
-      type: "list",
-      message:
-        "Which icon to resize?\n  - Inspect /output/icon-candidates and select the icon by its number\n",
-      choices: "12345".split(""),
-    },
-  ])
-  .then(({ iconToResize }) => {
-    new ImageResizer().resize(iconToResize);
-  });
+const iconPrompt = [
+  {
+    name: "iconToResize",
+    type: "list",
+    message:
+      "Which icon to resize?\n  - Inspect /output/icon-candidates and select the icon by its number\n",
+    choices: "12345".split(""),
+  },
+];
+
+(async () => {
+  const { iconToResize } = await inquirer.prompt(iconPrompt);
+  new ImageResizer().resize(iconToResize);
+})();
