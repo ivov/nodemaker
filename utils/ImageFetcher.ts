4770d4937db8d13abf478e7df3c316796aa673c6
Iván Ovejero <ivov.src@gmail.com>
2020-07-22 16:15:45 -0300

    Document ImageFetcher methods

--- utils/ImageFetcher.ts
+++ utils/ImageFetcher.ts
@@ -4,10 +4,12 @@ import fetch from "node-fetch";
 import { CUSTOM_SEARCH_API_URL } from "./constants";
 import config from "../config";
 
+/**Responsible for finding, fetching and saving to disk images for the icon candidates.*/
 export default class ImageFetcher {
   imageObject: any;
   imageLinks: string[] = [];
 
+  /**Fetch an image object from Google's Custom Search Engine based on the input query.*/
   async fetchImageObject(query: string) {
     const endpoint = `
     ${CUSTOM_SEARCH_API_URL}?
@@ -25,12 +27,14 @@ export default class ImageFetcher {
     this.imageObject = await response.json();
   }
 
+  /**Extract all the URLs of the image candidates in the image object.*/
   extractImageLinks() {
     this.imageObject.items.forEach((item: any) =>
       this.imageLinks.push(item.link)
     );
   }
 
+  /**Download and write to disk all the image candidates.*/
   downloadIconCandidates() {
     this.imageLinks.forEach(async (imageLink, index) => {
       const response = await fetch(imageLink);
