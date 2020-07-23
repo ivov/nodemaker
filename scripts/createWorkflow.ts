07fba71e99f05a258f60b1081f5b46926ca7d076
Iván Ovejero <ivov.src@gmail.com>
2020-07-23 13:24:18 -0300

    Add navigation until workflow edit page

--- scripts/createWorkflow.ts
+++ scripts/createWorkflow.ts
@@ -3,4 +3,6 @@ import WorkflowCreator from "../utils/WorkflowCreator";
 (async () => {
   await WorkflowCreator.init();
   await WorkflowCreator.doLogin();
+  await WorkflowCreator.accessEditScreen();
+  await WorkflowCreator.enterWorkflowDetails();
 })();
--- utils/WorkflowCreator.ts
+++ utils/WorkflowCreator.ts
@@ -15,13 +15,40 @@ export default class WorkflowCreator {
   public static async doLogin() {
     const { username, password } = config.n8n;
     const usernameSelector = 'input[placeholder="Username or email address"]';
-    const passwordSelector = 'input[placeholder="password"]';
+    const passwordSelector = 'input[placeholder="Password"]';
 
     await WorkflowCreator.page.goto(N8N_HOMEPAGE_URL);
     await WorkflowCreator.page.click("a[title='Login']");
 
+    await WorkflowCreator.page.waitForNavigation();
+
     await WorkflowCreator.page.type(usernameSelector, username);
     await WorkflowCreator.page.type(passwordSelector, password);
+    await WorkflowCreator.page.click("button[type='submit']");
+
+    await WorkflowCreator.page.waitForNavigation({ waitUntil: "networkidle0" }); // due to auth redirection
+  }
+
+  public static async accessEditScreen() {
+    await WorkflowCreator.page.click('a[href="/workflows/edit"]');
+    await WorkflowCreator.page.waitForNavigation({
+      waitUntil: "domcontentloaded",
+    });
+  }
+
+  public static async enterWorkflowDetails() {
+    const nameSelector =
+      'input[placeholder="The name the workflow should be published as"]';
+
+    const descriptionSelector = 'textarea[placeholder="Begin editing..."]';
+
+    const codeSelector = "code.language-json";
+
+    // await WorkflowCreator.page.type(nameSelector, "abc");
+    await WorkflowCreator.page.type(descriptionSelector, "def");
+    await WorkflowCreator.page.type(codeSelector, "ghi");
+
+    // await WorkflowCreator.page.click("button[type='submit']");
   }
 
   public static async close() {
