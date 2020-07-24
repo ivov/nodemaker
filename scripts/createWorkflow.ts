a8960a35e6a293572448e52c91a30dbcfe74d298
Iván Ovejero <ivov.src@gmail.com>
2020-07-24 17:37:34 -0300

    Complete workflow creation at n8n.io

--- scripts/createWorkflow.ts
+++ scripts/createWorkflow.ts
@@ -3,6 +3,5 @@ import WorkflowCreator from "../utils/WorkflowCreator";
 (async () => {
   await WorkflowCreator.init();
   await WorkflowCreator.doLogin();
-  await WorkflowCreator.accessEditScreen();
   await WorkflowCreator.enterWorkflowDetails();
 })();
