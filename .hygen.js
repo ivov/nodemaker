d4c809989e24c192901c425ac3359861b4764870
Iván Ovejero <ivov.src@gmail.com>
2020-07-28 16:08:53 -0300

    Document all helper functions

--- .hygen.js
+++ .hygen.js
@@ -1,5 +1,9 @@
 module.exports = {
   helpers: {
+    /**Format a string as a class name, uppercase for each initial and no whitespace.*/
+    classify: (name) => name.replace(/\s/g, ""),
+    /**Format a string as a lowercase single word, or a lowercase first word
+     * and uppercase initial + lowercase rest for following words.*/
     camelify: (input) => {
       const isSingleWord = input.split(" ").length === 1;
       const uppercaseInitialLowercaseRest = (input) =>
@@ -16,16 +20,22 @@ module.exports = {
       });
       return result.join("");
     },
+    /**Check if operations array has a Get All operation.*/
     hasGetAll: (operations) =>
       operations.some((operation) => operation.name === "Get All"),
-    getCredentialsString: (name, auth) =>
-      name + (auth === "OAuth2" ? "OAuth2" : "") + "Api",
+    /**Check if a particular operation is a GET request.*/
     isRetrieval: (operation) => operation.requestMethod === "GET",
+    /**Check if a particular operation has any associated additional fields.*/
     hasAdditionalFields: (operation) =>
       operation.fields.filter((field) => field.name === "Additional Fields"),
-    classify: (name) => name.replace(/\s/g, ""),
+    /**Create the credential string for based on auth type.*/
+    getCredentialsString: (name, auth) =>
+      name + (auth === "OAuth2" ? "OAuth2" : "") + "Api",
+    /**Check if the endpoint has an extractable variable.*/
     hasEndpointVariable: (endpoint) => endpoint.split("").includes("$"),
+    /**Extract the variable from the endpoint.*/
     getVariableFromEndpoint: (endpoint) => endpoint.match(/\$\$(.*)\$\$/)[1],
+    /**Reformat endpoint without variable boundary markers.*/
     fixEndpoint: (endpoint) =>
       endpoint
         .replace(/\/\$\$/, "/${")
