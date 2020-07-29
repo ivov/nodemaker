21c93587457ae958066f8346d98d6ac82fc4dab1
Erin <erin2722@gmail.com>
2020-07-29 11:20:25 -0400

    Trials

--- client/channels/ExampleChannel.ts
+++ client/channels/ExampleChannel.ts
@@ -2,10 +2,10 @@ import { IpcMainEvent } from "electron";
 import IpcChannel from "./IpcChannel.interface";
 import { metaParameters } from "../../parameters";
 
-export default class PrototypeChannel implements IpcChannel {
-  public name = "prototype-channel";
+export default class ExampleChannel implements IpcChannel {
+  public name = "example-channel";
 
   public async handle(event: IpcMainEvent) {
-    event.sender.send(this.name, metaParameters.serviceName);
+    event.sender.send(this.name, "hello my name is erin");
   }
 }
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,7 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld v-bind:msg="name" />
-    <h2>{{name2}}</h2>
+    <button v-on:click="name2">test 1</button>
+    <button v-on:click="sample">test 2</button>
+    <h3>{{name3}}</h3>
   </div>
 </template>
 
@@ -13,25 +15,42 @@ import HelloWorld from '../components/HelloWorld.vue';
 
 import Requester from '../../Requester';
 
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
+}
+
 @Component({
   components: {
     HelloWorld,
   },
   data: () => {
     return {
-      name: "erin123"
+      name: "erin123",
+      name3: "erin456"
     }
   },
-  computed: {
+  methods: {
     name2: async () => {
       const requester = new Requester();
-      const response = await requester.request<string>(
-        "prototype-channel"
-      );
+      console.log(requester);
+
+      const response = await requester.request(
+          "example-channel"
+        );
+      
       console.log(response);
-      return "hello world";
+    },
+    sample: async () => {
+      const response = await resolveAfter2Seconds();
+
+      console.log(response);
     }
   }
 })
+
 export default class App extends Vue {}
 </script>
