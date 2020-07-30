281a5805b1a0c34ad26c168c7286fbd1c1b77ad3
Erin <erin2722@gmail.com>
2020-07-30 15:23:46 -0400

    fixed config and completed frontend example

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,56 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld v-bind:msg="name" />
-    <button v-on:click="name2">test 1</button>
-    <button v-on:click="sample">test 2</button>
-    <h3>{{name3}}</h3>
+    <button @click="example()">call example</button>
+    <h4>{{tester}}</h4>
   </div>
 </template>
 
 <script lang="ts">
-/* eslint no-unused-vars: "warn" */
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '../components/HelloWorld.vue';
 
 import Requester from '../../Requester';
 
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  });
-}
 
 @Component({
-  components: {
-    HelloWorld,
-  },
   data: () => {
     return {
       name: "erin123",
-      name3: "erin456"
+      tester: ""
     }
   },
-  methods: {
-    name2: async () => {
+  computed: {
+    example() {
       const requester = new Requester();
-      console.log(requester);
-
-      const response = await requester.request(
-          "example-channel"
-        );
-      
-      console.log(response);
-    },
-    sample: async () => {
-      const response = await resolveAfter2Seconds();
-
-      console.log(response);
+      const response = requester.request("example-channel");
+      response.then((res:string) => {
+        console.log(res);
+        this.tester = res;
+      });
     }
   }
 })
 
 export default class App extends Vue {}
-</script>
+</script>n
