f398d5da0f234b4e89d2e1de646fe06a69b24e55
Iván Ovejero <ivov.src@gmail.com>
2020-08-10 15:46:17 -0300

    Add channel for package.json generation and types

--- client/channels/PackgenChannel.ts
+++ client/channels/PackgenChannel.ts
@@ -1,23 +1,14 @@
 import { IpcMainEvent } from "electron";
 import IpcChannel from "./IpcChannel.interface";
 import PackageJsonGenerator from "../../generators/PackageJsonGenerator";
-import sleep from "../../utils/sleep";
 
 export default class PackgenChannel implements IpcChannel {
-  public name = "docsgen-channel";
+  public name = "packgen-channel";
 
   public async handle(event: IpcMainEvent, metaParameters: MetaParameters) {
     process.chdir("..");
-
     const generator = new PackageJsonGenerator(metaParameters);
-    await generator.retrievePackageJson();
-
-    generator.insertCredentialPathIntoPackageJson();
-
-    await sleep(1000); // to ensure both insertions succeed
-
-    const result = await generator.insertNodePathIntoPackageJson();
-
+    const result = await generator.run();
     process.chdir("client");
 
     event.sender.send(this.name, result);
