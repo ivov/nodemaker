4a7397adcf22a4dfba3c2ae2bb1e8fa0fefa5bf1
Iván Ovejero <ivov.src@gmail.com>
2020-08-11 17:50:12 -0300

    Refactor workflow submission for consistency

--- scripts/createWorkflow.ts
+++ scripts/createWorkflow.ts
@@ -1,7 +1,14 @@
 import WorkflowCreator from "../services/WorkflowCreator";
 import { docsParameters } from "../parameters";
+import Highlighter from "../services/Highlighter";
 
 (async () => {
   const creator = new WorkflowCreator(docsParameters);
-  await creator.run();
+  const result = await creator.run();
+
+  Highlighter.showResult({
+    result,
+    successMessage: "Workflow submission on n8n.io successful.",
+    inspectMessage: true,
+  });
 })();
